831967eaff2494d1e083fb50f45abce9ed75d3ac
SrinivasVamshiM <98442269+SrinivasVamshiM@users.noreply.github.com>
2024-03-15 13:59:18 -0400

    Time: 552 ms (17.02%), Space: 21.8 MB (18.03%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        rand = 0
+        collist = []
+        while(rand != n):
+            list1 = []
+            for i in grid:
+                list1.append(i[rand])
+            collist.append(list1)
+            rand+= 1
+        count = 0
+        print(grid)
+        print(collist)
+        for j in range(n):
+            for k in range(n):
+                if grid[j] == collist[k]:
+                    count += 1
+        return count
+                
